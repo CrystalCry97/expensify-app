c599ba1fd068b7385a1e6cd1bea6dbe05131cb07
CrystalCry97 <amalsyahmi97@gmail.com>
2018-11-08 11:04:41 +0800

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ import 'react-dates/lib/css/_datepicker.css'; // see SingleDatePicker airbnb git
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500, createdAt: 500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
+// store.dispatch(addExpense({description: 'Water bill', amount: 4500, createdAt: 500}));
+// store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
+// store.dispatch(addExpense({description: 'Rent', amount: 109500}));
 
-const state= store.getState();
-const visibleExpense= getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpense);
+// const state= store.getState();
+// const visibleExpense= getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpense);
 
 const jsx =(
     <Provider store={store}>
